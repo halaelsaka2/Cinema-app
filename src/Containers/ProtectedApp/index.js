8b358a9f5d732d093769b0c8a3fcc856b8a3f2ff
Hala Elsaka <helsaka@qe-software.com>
2021-10-22 14:12:28 +0200

    add hash route

--- src/Containers/ProtectedApp/index.js
+++ src/Containers/ProtectedApp/index.js
@@ -3,13 +3,16 @@ import { Component } from "react";
 import { store, history } from "../../Redux/store";
 import Routes from "../Routes";
 import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
 
 class ProtectedApp extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <Routes history={history} />
-      </Provider>
+      <HashRouter>
+        <Provider store={store}>
+          <Routes history={history} />
+        </Provider>
+      </HashRouter>
     );
   }
 }
